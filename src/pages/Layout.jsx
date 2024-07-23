3ab1e495bccd7e4c41c7d81af798fa4a8891982f
jinho9482 <157558300+jinho9482@users.noreply.github.com>
2024-07-23 17:15:20 +0900

    [Feat] Add logo and Navbar with basic setting

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import waguwagu from "../assets/waguwagu.png"
+
+const Layout = () => {
+  return (
+    <>
+      <header>
+        <div className="logo" style={{ textAlign: "center", margin: "30px 0" }}>
+            <img src={waguwagu} alt="waguwagu" />
+        </div>
+        <NavBar />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default Layout;

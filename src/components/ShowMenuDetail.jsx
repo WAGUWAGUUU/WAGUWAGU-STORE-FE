9695ea390ea3e11481dfef32a8342ff4211d4f3a
Kim Sehyun <kkshyun56@gmail.com>
2024-07-25 09:19:41 +0900

    [feat] : my-menu-detail

--- src/components/ShowMenuDetail.jsx
+++ src/components/ShowMenuDetail.jsx
@@ -41,76 +41,142 @@ const ShowMenuDetail = () => {
       style={{ width: window.innerWidth / 2.1 }}
     >
       {menuInfo ? (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <img
-            width={window.innerWidth / 3}
-            height="200px"
-            style={{ margin: "20px" }}
-            src={tteokbokki}
-          />
+        <div>
           <div
-            key={menuInfo.menuId}
             style={{
-              width: window.innerWidth / 3,
-              borderColor: "#94D35C",
-              borderWidth: 5,
-              borderStyle: "dotted",
-              borderTopLeftRadius: "50px",
-              borderTopRightRadius: "50px",
-              borderEndEndRadius: "50px",
-              paddingLeft: "50px",
-              padding: "20px",
-              margin: "5px",
               display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <div>
-              {menuInfo.menuPossible ? (
-                <p style={{ fontSize: 20 }}>{menuInfo.menuName}</p>
-              ) : (
-                <p style={{ fontSize: 20, color: "red" }}>
-                  [주문 막아놓음] {menuInfo.menuName}
-                </p>
-              )}
-              <p
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "20px",
+              }}
+            >
+              <img
+                width={window.innerWidth / 3}
+                height="200px"
+                //   style={{ margin: "20px" }}
+                src={tteokbokki}
+              />
+              <button
                 style={{
-                  marginTop: 5,
-                  color: "#757575",
+                  borderStyle: "solid",
+                  borderColor: "#94D35C",
+                  padding: "0px 7px",
+                  borderWidth: 3,
+                  marginTop: "10px",
+                  alignSelf: "flex-end",
                 }}
               >
-                {menuInfo.menuIntroduction}
-              </p>
-              <p style={{ marginTop: 10 }}>가격 {menuInfo.menuPrice}원</p>
+                사진 수정
+              </button>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "20px",
+              }}
+            >
+              <div
+                key={menuInfo.menuId}
+                style={{
+                  width: window.innerWidth / 3,
+                  borderColor: "#94D35C",
+                  borderWidth: 5,
+                  borderStyle: "dotted",
+                  borderTopLeftRadius: "50px",
+                  borderTopRightRadius: "50px",
+                  borderEndEndRadius: "50px",
+                  paddingLeft: "50px",
+                  padding: "20px",
+                  margin: "5px",
+                  display: "flex",
+                }}
+              >
+                <div>
+                  {menuInfo.menuPossible ? (
+                    <p style={{ fontSize: 20 }}>{menuInfo.menuName}</p>
+                  ) : (
+                    <p style={{ fontSize: 20, color: "red" }}>
+                      [주문 막아놓음] {menuInfo.menuName}
+                    </p>
+                  )}
+                  <p
+                    style={{
+                      marginTop: 5,
+                      color: "#757575",
+                    }}
+                  >
+                    {menuInfo.menuIntroduction}
+                  </p>
+                  <p style={{ marginTop: 10 }}>가격 {menuInfo.menuPrice}원</p>
+                </div>
+              </div>
+              <button
+                style={{
+                  borderStyle: "solid",
+                  borderColor: "#94D35C",
+                  padding: "0px 7px",
+                  borderWidth: 3,
+                  marginTop: "10px",
+                  alignSelf: "flex-end",
+                }}
+              >
+                메뉴 수정
+              </button>
             </div>
           </div>
-          {/* {optionLists ? (
-            optionLists.map((optionList) => {
-              <div key={optionList.listName}>
-                <p>{optionList.listName}</p>
-                {optionList.options ? (
-                  optionList.options.map((option) => {
-                    <div key={option.optionId}>
-                      <p>{option.optionTitle}</p>
-                      <p>{option.optionPrice}원</p>
-                    </div>;
-                  })
-                ) : (
-                  <p>텅!</p>
-                )}
-              </div>;
-            })
-          ) : (
-            <div></div>
-          )} */}
+          <div
+            style={{ display: "flex", flexDirection: "column", margin: "20px" }}
+          >
+            {optionLists ? (
+              optionLists.map((optionList, index) => (
+                <div
+                  key={index}
+                  style={{
+                    marginTop: "20px",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                  }}
+                >
+                  <p
+                    style={{
+                      fontSize: 18,
+                      color: "#757575",
+                    }}
+                  >
+                    {optionList.listName}
+                  </p>
+                  {optionList.options ? (
+                    optionList.options.map((option) => (
+                      <div
+                        key={option.optionId}
+                        style={{ padding: "20px 40px", display: "flex" }}
+                      >
+                        <p>{option.optionTitle}</p>
+                        <p>{option.optionPrice}원</p>
+                      </div>
+                    ))
+                  ) : (
+                    <p>옵션이 없습니다</p>
+                  )}
+                </div>
+              ))
+            ) : (
+              <div style={{ fontSize: 18, color: "#757575" }}>
+                옵션 리스트가 없습니다.
+              </div>
+            )}
+          </div>
         </div>
       ) : (
-        <p>텅!</p>
+        <p>메뉴 정보가 없습니다</p>
       )}
     </div>
   );

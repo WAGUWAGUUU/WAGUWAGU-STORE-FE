4cb8901c854009996c446a3acea1eaf6be3cb2ba
Kim Sehyun <kkshyun56@gmail.com>
2024-08-20 20:13:49 +0900

    [fix] : api

--- src/api/Option.js
+++ src/api/Option.js
@@ -1,10 +1,14 @@
 import { api } from "../config/network";
 
 export const saveOption = async (optionListId, optionRequest) => {
-    await api(`/api/v1/options/list/${optionListId}`, "post", optionRequest);
-}
+  await api(
+    `/api/v1/store/options/list/${optionListId}`,
+    "post",
+    optionRequest
+  );
+};
 
 export const getOptions = async (data) => {
-    const res = await api('/api/v1/options/list/' + data, "get");
-    return res;
-}
+  const res = await api("/api/v1/store/options/list/" + data, "get");
+  return res;
+};
